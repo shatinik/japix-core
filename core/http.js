e332605205f79a26c7cc5dccc8d4a6575b78b646
@shati <rexpower@tut.by>
2018-11-20 23:40:32 +0300

    [General] Added support of body data chunks

--- core/http.js
+++ core/http.js
@@ -1,43 +1,57 @@
 const http = require('http');
 const url = require('url');
 const Router = require('./router/router');
-
+const bodyParserLoader = require('./utils/bodyparser');
 class Http {
     /**
      * @private
      */
-    static async init(routesTable, props) {   
+    static async init(routesTable, props) {
         let requestUrl = '';
+        let server = new http.Server();
 
-        return http.createServer(async (request, response) => {
-            requestUrl = url.parse(request.url);
-
-            if (requestUrl.pathname === '/favicon.ico' && props.blockFavicon === true) {
-                return;
-            }
-            let action = await Router.findAction(routesTable, requestUrl.pathname);
+        server.on('request', async (req, res) => {
+            res.setHeader('content-type', 'application/json');
+            let bodyRaw = '';
             let result = {};
-            let status = 200;
-            if (!action) {
-                status = 404;
-            } else {
-                if (request.method.toLowerCase() === action.method) {
-                    let routeData = await Router.getRouteData({
-                        model: action.data,
-                        body: {},
-                        requestUrl: requestUrl
-                    });
-                    result = await action.action(routeData);
-                } else {
-                    status = 404;
-                }
-            }
-            console.log(status);
-            response.writeHead(status, {
-                'content-type': 'application/json'
+            req.on('data', (chunk) => {
+                bodyRaw += chunk.toString();
             });
-            response.end(JSON.stringify(result));
+            req.on('end', async () => {
+                try {
+                    let parse = bodyParserLoader(req.headers['content-type']);
+                    let body = parse(bodyRaw);
+                    requestUrl = url.parse(req.url);
+        
+                    if (requestUrl.pathname === '/favicon.ico' && props.blockFavicon === true) {
+                        return;
+                    }
+                    let action = await Router.findAction(routesTable, requestUrl.pathname);
+                    if (!action) {
+                        res.statusCode = 404;
+                    } else {
+                        if (req.method.toLowerCase() !== action.method) {
+                            res.statusCode = 404;
+                        } else {
+                            let routeData = await Router.getRouteData({
+                                model: action.data,
+                                body: body,
+                                requestUrl: requestUrl
+                            });
+                            result = await action.action(routeData);
+                        }
+                    }
+                    res.end(JSON.stringify(result));
+                } catch (e) {
+                    res.statusCode = 500;
+                    result = {'message': 'Internal Server Error'};
+                    res.end(JSON.stringify(result));
+                    throw e;
+                }
+            });
         });
+
+        return server;
     }
 
     /**
