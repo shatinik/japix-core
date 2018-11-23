e47962aa69c3df86a75a5e67beee58b2c2be81ba
@shati <rexpower@tut.by>
2018-11-23 12:33:49 +0300

    [General] Added event emmiter for 'error' event on HTTP

--- core/http.js
+++ core/http.js
@@ -11,27 +11,42 @@ class Http {
         let server = new http.Server();
 
         server.on('request', async (req, res) => {
-            res.setHeader('content-type', 'application/json');
-            let bodyRaw = '';
             let result = {};
+            res.setHeader('content-type', 'application/json');
+            
+            let bodyRaw = '';
             req.on('data', (chunk) => {
                 bodyRaw += chunk.toString();
             });
+
+            req.on('error', e => {
+                result = { 'message': e.message };
+                res.end(JSON.stringify(result));
+                console.error(e);
+            });
             req.on('end', async () => {
                 try {
+                    let authstring = new Buffer((req.headers.authorization || '').split(' ')[1] || '', 'base64').toString();
+                    if (authstring !== 'test:1337') {
+                        res.statusCode = 401;
+                        res.setHeader('WWW-Authenticate', 'Basic realm="nope"');
+                        throw new Error('Wrong auth data');
+                    }
                     let parse = bodyParserLoader(req.headers['content-type']);
                     let body = parse(bodyRaw);
                     requestUrl = url.parse(req.url);
-        
+
                     if (requestUrl.pathname === '/favicon.ico' && props.blockFavicon === true) {
                         return;
                     }
                     let action = await Router.findAction(routesTable, requestUrl.pathname);
                     if (!action) {
                         res.statusCode = 404;
+                        throw new Error('Page not found');
                     } else {
                         if (req.method.toLowerCase() !== action.method) {
                             res.statusCode = 404;
+                            throw new Error('Page not found');
                         } else {
                             let routeData = await Router.getRouteData({
                                 model: action.data,
@@ -43,10 +58,9 @@ class Http {
                     }
                     res.end(JSON.stringify(result));
                 } catch (e) {
-                    res.statusCode = 500;
-                    result = {'message': 'Internal Server Error'};
+                    result = { 'message': e.message };
                     res.end(JSON.stringify(result));
-                    throw e;
+                    req.emit('error', e);
                 }
             });
         });
